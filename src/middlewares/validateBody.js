bb0c054f339f1243d354e45fa07e4e7b6db1dc21
Lili <Liliia_Nel@proton.me>
2025-08-16 09:36:31 +0200

    clear console logs

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -4,7 +4,6 @@ export default function validateBody (schema) {
     return async (req, res, next)=>{
         try {
             const result = await schema.validateAsync(req.body, {abortEarly: false});
-            console.log(result);
             next();
         } catch (error) {
             const errors = error.details.map(detail => detail.message);
